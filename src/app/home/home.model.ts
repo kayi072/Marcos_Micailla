304a42f3640ef9df6f4e515cf2e2f9df45ccff84
kayi072 <marcosmicailla639@gmail.com>
2024-06-08 02:08:11 +0800

    act 10 commit

--- src/app/home/home.model.ts
+++ src/app/home/home.model.ts
@@ -1,4 +1,4 @@
-export class User{
+export class User {
     id: string;
     firstname: string;
     lastname: string;
@@ -6,27 +6,30 @@ export class User{
     section: number[];
     dateEnrolled: Date;
     gwa: number;
-    isGraduated: boolean = false ;
-    
-   
-    constructor(id: string = '', firstname: string = '', lastname: string = '',
-                                course: string[] = [], section: number[] = [], dateEnrolled: Date = new Date(),
-                                 gwa: number = 0, isGraduated: boolean = false){
-    
-    this.id = id;
-    this.firstname = firstname;
-    this.lastname = lastname;
-    this.course = course;
-    this.section = section;
-    this.dateEnrolled = dateEnrolled;
-    this.gwa = gwa;
-    this.isGraduated = isGraduated;
-    
-     }
-}
-
-export interface iUser{
-
+    isGraduated: boolean = false;
+  
+    constructor(
+      id: string = '',
+      firstname: string = '',
+      lastname: string = '',
+      course: string[] = [],
+      section: number[] = [],
+      dateEnrolled: Date = new Date(),
+      gwa: number = 0,
+      isGraduated: boolean = false
+    ) {
+      this.id = id;
+      this.firstname = firstname;
+      this.lastname = lastname;
+      this.course = course;
+      this.section = section;
+      this.dateEnrolled = dateEnrolled;
+      this.gwa = gwa;
+      this.isGraduated = isGraduated;
+    }
+  }
+  
+  export interface iUser {
     id: string;
     firstname: string;
     lastname: string;
@@ -35,6 +38,5 @@ export interface iUser{
     dateEnrolled: Date;
     gwa: number;
     isGraduated: boolean;
-}
-
-
+  }
+  
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { User, iUser } from './home.model';
+import { HomeService } from './home.service';
+import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
-import { AlertController, ToastController } from '@ionic/angular';
-import { AuthenticationService } from '../auth.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -9,20 +11,61 @@ import { AuthenticationService } from '../auth.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  user: User = new User();
+  userList : iUser[] = [];
+  isLoading = false;
 
-  constructor(
-    private authenticationService: AuthenticationService,
-    private router: Router,
-    private alertController: AlertController,
-    private toastController: ToastController
-  ) {}
-
-  ngOnInit() {
+  constructor( private homeService: HomeService,
+  private alertController: AlertController,
+  private router: Router, private auth: AuthService
+  ) {
+  
   }
 
-  dashboard(): void {
-    this.authenticationService.canProceed = true;
-    this.router.navigate(['/dashboard']);
+  ionViewWillEnter(){
+    this.users();
   }
 
+save(){
+  if(this.user.id){
+    this.homeService.updateData(this.user);
+    this.homeService.presentAlert('Update', 'User Updated');
+  } else {
+    this.homeService.addData(this.user);
+    this.homeService.presentAlert('Success', 'User Added');
+  }
+  this.user = new User();
+  this.users();
+}
+
+async users(){
+  this.isLoading = true;
+  this.userList = await this.homeService.getUsers();
+  this.homeService.newUserList = this.userList;
+  this. isLoading = false;
+}
+
+edit(user: iUser){
+  this.user = user;
+}
+
+async delete(user: User){
+  this.isLoading = true;
+  await this.homeService.deleteData(user);
+  this.homeService.presentAlert('Delete', 'User Deleted');
+  this.users();
+  this.user = new User();
+  this.isLoading = false;
+}
+
+update(user: User) {
+  this.router.navigate(['update', user.id]);
+  this.homeService.newUserList = this.userList;
+  this.edit(user);
+}
+
+logOut() {
+  this.router.navigate(['login']);
+  this.auth.setAuthentication(false);
+}
 }
